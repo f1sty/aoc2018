52aa617e68356aff3e70c837777b9493a7d2cf8d
Yurii Skrynnykov <truef1s7@gmail.com>
2019-02-06 11:53:09 +0200

    Day1/part2 solution

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,5 +1,4 @@
 defmodule Aoc2018.Day1 do
-
   def calibrate_freq(input_file) do
     File.stream!(input_file)
     |> calibrate()
@@ -8,5 +7,29 @@ defmodule Aoc2018.Day1 do
   def calibrate(input) do
     input
     |> Enum.reduce(0, &(&2 + String.to_integer(String.trim(&1))))
+  end
+
+  def reaches_twice(input) do
+    reaches_twice(input, input)
+  end
+
+  defp reaches_twice(rest, initial_input, curr_freq \\ 0, seen_before \\ %{})
+
+  defp reaches_twice([], inp, curr_freq, seen_before) do
+    reaches_twice(inp, inp, curr_freq, seen_before)
+  end
+
+  defp reaches_twice([num | tail], inp, curr_freq, seen_before) do
+    num =
+      num
+      |> String.trim()
+      |> String.to_integer()
+
+    if Map.has_key?(seen_before, curr_freq) do
+      curr_freq
+    else
+      seen_before = Map.put(seen_before, curr_freq, true)
+      reaches_twice(tail, inp, curr_freq + num, seen_before)
+    end
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,14 +2,26 @@ defmodule Aoc2018.Day1.Test do
   use ExUnit.Case
   alias Aoc2018.Day1
 
-  test "testing with example inputs" do
-    assert Day1.calibrate(~w/+1 -2 +3 +1/) == 3
-    assert Day1.calibrate(~w/+1 +1 +1/) == 3
-    assert Day1.calibrate(~w/+1 +1 -2/) == 0
-    assert Day1.calibrate(~w/-1 -2 -3/) == -6
+  describe "part 1" do
+    test "testing with example inputs" do
+      assert Day1.calibrate(~w/+1 -2 +3 +1/) == 3
+      assert Day1.calibrate(~w/+1 +1 +1/) == 3
+      assert Day1.calibrate(~w/+1 +1 -2/) == 0
+      assert Day1.calibrate(~w/-1 -2 -3/) == -6
+    end
+
+    test "testing with generated input" do
+      assert Day1.calibrate_freq("lib/day1/input.txt") == 437
+    end
   end
 
-  test "testing with generated input" do
-    assert Day1.calibrate_freq("lib/day1/input.txt") == 437
+  describe "part 2" do
+    test "testing with example inputs" do
+      assert Day1.reaches_twice(~w/+1 -2 +3 +1/) == 2
+      assert Day1.reaches_twice(~w/+1 -1/) == 0
+      assert Day1.reaches_twice(~w/+3 +3 +4 -2 -4/) == 10
+      assert Day1.reaches_twice(~w/-6 +3 +8 +5 -6/) == 5
+      assert Day1.reaches_twice(~w/+7 +7 -2 -7 -4/) == 14
+    end
   end
 end
